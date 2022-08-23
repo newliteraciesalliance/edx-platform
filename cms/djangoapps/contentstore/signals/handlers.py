114cf455cb2c729e9654b89ab2ee86fca179bc9a
Tim McCormack <tmccormack@edx.org>
2022-08-23 16:26:46 +0000

    fixup! Use edx_event_bus_kafka directly
    This direct reference is now possible with v0.4.1, but we'll be removing
    it again later once we have configurable loading.

--- cms/djangoapps/contentstore/signals/handlers.py
+++ cms/djangoapps/contentstore/signals/handlers.py
@@ -11,6 +11,7 @@ from django.core.cache import cache
 from django.db import transaction
 from django.dispatch import receiver
 from edx_toggles.toggles import SettingToggle
+from edx_event_bus_kafka.publishing.event_producer import send_to_event_bus
 from opaque_keys.edx.keys import CourseKey
 from openedx_events.content_authoring.data import CourseCatalogData, CourseScheduleData
 from openedx_events.content_authoring.signals import COURSE_CATALOG_INFO_CHANGED
@@ -155,13 +156,6 @@ def listen_for_course_catalog_info_changed(sender, signal, **kwargs):
     """
     Publish COURSE_CATALOG_INFO_CHANGED signals onto the event bus.
     """
-    try:
-        # FIXME: This needs to be replaced with a configurable loading
-        # mechanism built into openedx-events.
-        # See https://github.com/openedx/openedx-events/pull/85)
-        from edx_event_bus_kafka.publishing.event_producer import send_to_event_bus
-    except ImportError:
-        return
     send_to_event_bus(
         signal=COURSE_CATALOG_INFO_CHANGED, topic='course-catalog-info-changed',
         event_key_field='catalog_info.course_key', event_data={'catalog_info': kwargs['catalog_info']}
